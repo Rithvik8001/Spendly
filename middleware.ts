20b07d2833f27d7f1433ea438c7bf2625b659353
YourUsername <rithvik.utd@outlook.com>
2024-10-08 16:21:42 -0400

    bug fixes-4

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/api/webhooks/clerk"],
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
